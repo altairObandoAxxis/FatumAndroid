ebb533095f09c39d973402d7cc06719ce0a39601
Noel Obando <noel.obando@axxis-systems.com>
2024-04-01 13:19:20 -0600

    android notification config

--- Views/Home.jsx
+++ Views/Home.jsx
@@ -6,11 +6,15 @@ import { View } from 'react-native';
 import { Icon } from '@rneui/themed';
 import { GetStartedView } from './GetStarted';
 import { DoCmd } from '../Api/doCmd';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { PolicyView } from './PolicyView';
 import { ClaimView } from './ClaimView';
 import { PremiumView } from './PremiumView';
 import { AccountView } from './AccountView';
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,8 +25,51 @@ const formatBadge =( value ) => {
     return count > 99 ? '+99' : count.toString()
 }
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+});
+
+async function registerForPushNotificationsAsync() {
+    let token;
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('fatum', {
+        name: 'fatum',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId,
+      });
+      console.log(token.data);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    return token.data;
+  }
+
 export const Home =()=>{
     const insets = useSafeAreaInsets();
+    const [ expoPushToken, setExpoPushToken] = useState('');
     const { userData, setUserData } = useUserData();
     const [ ContactData, setData ]  = useState();
     const { theme } = useTheme();
@@ -47,6 +94,22 @@ export const Home =()=>{
         setUserData({...userData, 
         refreshBadges: fetchUserData })
     },[userData.token])
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    
+        //notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        //  console.log(notification);
+        //});
+    
+       //responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+       //  console.log(response);
+       //});
+    
+       //return () => {
+       //  Notifications.removeNotificationSubscription(notificationListener.current);
+       //  Notifications.removeNotificationSubscription(responseListener.current);
+       //};
+    }, []);
 
     return <View style={{ paddingTop: insets.top, flex: 1, backgroundColor: 'white' }}>
         <Tab.Navigator 
